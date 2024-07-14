854bffcb369e37cde9ff2cffc8c317f39cfcb260
krunal-chaudhari-developer <chaudharikrunal825@gmail.com>
2024-07-14 10:12:09 +0530

    The task is completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { MdDelete, MdEdit } from "react-icons/md";
+import Navbar from "./Components/Navbar";
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [showFinished, setshowFinished] = useState(false);
+
+  useEffect(() => {
+    let todosString = localStorage.getItem("todos");
+    if (todosString) {
+      let todos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished);
+  };
+
+  const handleEdit = (e, id) => {
+    let t = todos.filter((i) => i.id === id);
+    setTodo(t[0].todo);
+
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    setTodo("");
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return item.id === id;
+    });
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="md:container mx-3 md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
+        <h1 className="font-bold text-center text-xl">
+          i-Task - Manage your tools at one place
+        </h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <div className="flex ">
+            <input
+              onChange={handleChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleAdd();
+                }
+              }}
+              value={todo}
+              type="text"
+              className="w-full rounded-full px-5 py-1"
+            />
+            <button
+              onClick={handleAdd}
+              disabled={todo.length <= 3}
+              className="bg-violet-800 disabled:bg-violet-300 hover:bg-violet-950 p-2 py-1 mx-2 text-sm font-bold text-white rounded-lg"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+        <input
+          onChange={toggleFinished}
+          type="checkbox"
+          checked={showFinished}
+          className="my-4"
+        />{" "}
+        Show Finished
+        <div className="border border-black opacity-20 my-4" />
+        <h2 className="text-lg font-bold">Your Todos</h2>
+        <div className="todos">
+          {todos.length === 0 && (
+            <div className="text-5xl sm:text-7xl font-semibold text-indigo-200 text-center py-20 opacity-50">
+              No Todos to display...
+            </div>
+          )}
+          {todos.map((item) => {
+            return (
+              (showFinished || !item.isCompleted) && (
+                <div key={item.id} className="todo flex my-3 justify-between ">
+                  <div className="flex gap-5">
+                    <input
+                      onChange={handleCheckbox}
+                      type="checkbox"
+                      checked={item.isCompleted}
+                      name={item.id}
+                      id=""
+                    />
+                    <div className={item.isCompleted ? "line-through" : ""}>
+                      {item.todo}
+                    </div>
+                  </div>
+                  <div className="buttons flex h-full">
+                    <button
+                      onClick={(e) => {
+                        handleEdit(e, item.id);
+                      }}
+                      className="bg-violet-800 hover:bg-violet-950 px-3 py-1.5 text-sm font-bold text-white rounded-md mx-1"
+                    >
+                      <MdEdit />
+                    </button>
+                    <button
+                      onClick={(e) => {
+                        handleDelete(e, item.id);
+                      }}
+                      className="bg-violet-800 hover:bg-violet-950 px-3 py-1.5 text-sm font-bold text-white rounded-md mx-1"
+                    >
+                      <MdDelete />
+                    </button>
+                  </div>
+                </div>
+              )
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
